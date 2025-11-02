c644cdd01246e00ff5c4c0ac165636147fb0c41d
Abeda_Begum <skabeda82@gmail.com>
2025-11-02 18:12:15 +0530

    Change API_URL to production endpoint
    Updated API_URL to point to the production server.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "https://msd-backend-crhk.onrender.com";
 
 // Get token from localStorage
 const getAuthHeader = () => {
